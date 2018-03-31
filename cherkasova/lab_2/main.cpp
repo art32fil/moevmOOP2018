80fa573466942a3ebd5bd947b6f725b95e376cfa
cherowl <cherowl@yandex.ru>
2018-04-01 02:16:58 +0300

    problems with List

--- cherkasova/lab_2/main.cpp
+++ cherkasova/lab_2/main.cpp
@@ -22,9 +22,26 @@ int menu()
     }
 }
 
+// bool check_for_win(Field &fd){
+//     int red_total = fd.getconts()[0].get_size();
+//     int green_total = fd.getconts()[1].get_size();    
+//     int flag_redkilled = 0;
+//     int flag_greenkilled = 0;
+//     for(int i = 0; i < x_size_; i++){
+//        for(int j = 0; j < y_size_; j++){
+//             if(is_obj(i, j) != 0) break; // 'o' or '.'
+//             else flag_redkilled = 1;
+//     }
+//     for(auto elem: fd.getconts()[1]){
+//         if(alive(elem)) return false;
+//     }
+//     return true;
+// }
+
 //-----------------MAIN----------------------------
 int main()
 {
+    // cout << type_info((fd.getconts()[0].find_ob_index(1,1).get()./.name();
     filebuf filename;
     auto flagfile = filename.open("input", ios::in);
     if(!flagfile){ cout << " ERROR with file!\n "; exit(1); }
@@ -59,7 +76,7 @@ int main()
         cin >> fd;    
     }
 
-    // cout << fd;
+//fix while
     int flag = 0;
     cout << "See all containers (army: [ object_index x y hp] ):\n"; fd.print_cont();
     while(1){
@@ -75,7 +92,7 @@ int main()
             ind = fd.find_ob_index(x,y);
             if(ind == -1 || a == -1) { cout << "Selected target wasn't found. Try again (maybe inccorect input)." <<endl; 
                     cin.clear(); continue;}
-            else if (fd.is_obj(x,y)) {
+            else if (fd.is_obj(x,y) == 1) {
                 {cout << "Yes: hp = "<< fd.getconts()[a]->find(ind)->get().gethp() << endl; flag = 1; break;}
             } else {cout << "No: doesn't exist or was killed.\n"; flag = 1;}
             if(flag) break;
@@ -83,13 +100,13 @@ int main()
 
     flag = 0;
         while(1){
-            cout << "Input coordinates of the target (unsigned int numbers separated by space) to select and get to know its army (q to exit):\n";
-            x = 0; cin >> x;
-            if ( x == 'q') { fd.~Field(); filename.close(); exit(0);}
-            cin >> y;             
+            // cout << "Input coordinates of the target (unsigned int numbers separated by space) to select and get to know its army (q to exit):\n";
+            // x = 0; cin >> x;
+            // if ( x == 'q') { fd.~Field(); filename.close(); exit(0);}
+            // cin >> y;             
 
-            int belong = fd.belonging_army(x,y);
-            switch(belong){
+            // int belong = fd.belonging_army(x,y);
+            switch(a){
                 case 0:
                     cout << "Selected target is red\n";
                     flag = 1; break;
@@ -121,6 +138,9 @@ int main()
         else if (switch_on == 'q') { fd.~Field(); filename.close(); exit(0);}
         else { cout << "ERROR! Incorrect input, try again.\n";  cinclear(); continue; }
     }
+    // if(check_for_win == 0) {cout << "The red army won"; break; }
+    // else if(check_for_win == 1){cout << "The green army won"; break;}
+    // else continue;
 }
     
     fd.~Field();
