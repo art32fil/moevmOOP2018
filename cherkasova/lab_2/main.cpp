4e6c2f22ebfb31e8f98d47c1c1a2dc97a3e875bd
cherowl <cherowl@yandex.ru>
2018-04-04 14:51:06 +0300

    green army problem

--- cherkasova/lab_2/main.cpp
+++ cherkasova/lab_2/main.cpp
@@ -25,21 +25,25 @@ int menu()
     }
 }
 
-// bool check_for_win(Field &fd){
-//     int red_total = fd.getconts()[0].get_size();
-//     int green_total = fd.getconts()[1].get_size();    
-//     int flag_redkilled = 0;
-//     int flag_greenkilled = 0;
-//     for(int i = 0; i < x_size_; i++){
-//        for(int j = 0; j < y_size_; j++){
-//             if(is_obj(i, j) != 0) break; // 'o' or '.'
-//             else flag_redkilled = 1;
-//     }
-//     for(auto elem: fd.getconts()[1]){
-//         if(alive(elem)) return false;
-//     }
-//     return true;
-// }
+int check_for_win(Field &fd){
+// 0 - red won, 1- green won, -1 - continue game
+    int red_total = fd.getconts()[0]->get_size(); //cout<<"red_ "<<red_total<<endl;
+    int green_total = fd.getconts()[1]->get_size();    
+
+    for(auto& elem: *(fd.getconts()[0])){
+        if(!elem.get()->alive()) 
+            red_total--;
+    }
+    for(auto& elem: *(fd.getconts()[1])){
+        if(!elem.get()->alive()) 
+            green_total--;
+    }   
+    if(!green_total) 
+            return 1;
+    if(!red_total) 
+            return 0;
+    return -1; // continue gamme   
+}
 
 //-----------------MAIN----------------------------
 int main()
@@ -48,71 +52,45 @@ int main()
     auto flagfile = filename.open("input", ios::in);
     if(!flagfile){ cout << " ERROR with file!\n "; exit(1); }
     istream fin(&filename);
+    //ifstream fin("file");
    
     int x_load = 0;
     int y_load = 0;
     int army_number = 0;
-    
-    /* int choice = menu();
-    if(choice){ // file
-        fin >> x_load >> y_load;   
-    }  
-    else { //console
-        cout << "Enter size of field (x y) (unsigned int numbers separated by space): " <<endl;
-        cin >> x_load >> y_load;
-        int m = 0; 
-        while(m++ < 2){
-            cout << "_" << m << " army: " << endl;
-            cout << "Enter the amount of objects (unsigned int)\n"; 
-            cout << "Then - their location (x y) and hit points (unsigned int numbers separated by space): \n";
-            cout << "Repeat the same actions for the second army. \n";
-// fix
-        }
-    }
-
-    Field fd(x_load, y_load);
-    if(choice){ // file
-    }  
-    else { //console
-        cin >> fd;    
-    } */
-    
+       
     Field fd;
     fd.read_from_file(fin);   
-    fin >> fd;
     fd.print_field(cout);
-    
 
-//fix while
     int flag = 0;
+    int flag_exit = 0;
     cout << "See all containers - army: [ object_index x y hp] :\n"; fd.print_cont();
-    while(1){
-        int a, x, y, ind;
-        while(1){
+    while(!flag_exit){
+        int a = 0, x = 0, y = 0, ind = 0;
+        char c[1];
+        int flag_finish = 0;
+        while(!flag_finish){
             cinclear();
             cout << "If there is alive object? Enter coordinates of the object (x y - unsigned int numbers separated by space) or q to exit: " << endl;
-            flag = 1;
-            cin >> x;
-            if ( x == 'q') { fd.~Field(); filename.close(); exit(0);}    
+            // flag = 1;
+            cin >> c[0];
+            if ( *c == 'q') {flag_exit = 1; break;}    
+            x = atoi(c); 
             cin >> y;
             a = fd.belonging_army(x,y);
             ind = fd.find_ob_index(x,y);
             if(ind == -1 || a == -1) { cout << "Selected target wasn't found. Try again (maybe inccorect input)." <<endl; 
                     cin.clear(); continue;}
             else if (fd.is_obj(x,y) == 1) {
-                {cout << "Yes: hp = "<< fd.getconts()[a].find(ind)->get().gethp() << endl; flag = 1; break;}
+                cout << "Yes: hp = "<< fd.get_ob(x,y)->gethp() << endl; 
+                flag = 1; 
+                break;
             } else {cout << "No: doesn't exist or was killed.\n"; flag = 1;}
             if(flag) break;
         }
-
+    if(flag_exit) break;
     flag = 0;
         while(1){
-            // cout << "Input coordinates of the target (unsigned int numbers separated by space) to select and get to know its army (q to exit):\n";
-            // x = 0; cin >> x;
-            // if ( x == 'q') { fd.~Field(); filename.close(); exit(0);}
-            // cin >> y;             
-
-            // int belong = fd.belonging_army(x,y);
             switch(a){
                 case 0:
                     cout << "Selected target is red\n";
@@ -131,27 +109,33 @@ int main()
     cout << "Input a damage value (unsigned int): "; 
     cin >> d; 
     ind = fd.find_ob_index(x,y);
-    fd.getconts()[a].find(ind)->get().damage(d);
-    cout << "Hit points left in selected target: " << fd.getconts()[a].find(ind)->get().gethp() <<endl;
+    fd.get_ob(x,y)->damage(d);
+    fd.is_obj(x,y); //dekete killed object if such have found
+    cout << "Hit points left in selected target: " << fd.get_ob(x,y)->gethp() <<endl;
 
     cout << fd;
+    
+    int check = check_for_win(fd);
+    if(check == 1) {cout << "\n!!!The red army won!!!\n"; break; }
+    else if(check == 0){cout << "\n!!!The green army won!!!\n"; break;}
+    else continue;
 
     while(1){
-        cinclear();
-        cout << "Continue or quit? (c/q)\n";
+        // cinclear();
+        cout << "Continue or quit? (c/q): ";
         char switch_on;
         cin >> switch_on; 
         if (switch_on == 'c') break;
-        else if (switch_on == 'q') { fd.~Field(); filename.close(); exit(0);}
-        else { cout << "ERROR! Incorrect input, try again.\n";  cinclear(); continue; }
+        else if (switch_on == 'q') { flag_exit = 1; break;}
+        else { cout << "ERROR! Incorrect input, try again.\n"; cinclear(); continue; }
     }
-    // if(check_for_win == 0) {cout << "The red army won"; break; }
-    // else if(check_for_win == 1){cout << "The green army won"; break;}
-    // else continue;
 }
     
-    fd.~Field();
+    // fd.~Field();
     filename.close();
+    // file.close();
     return 0;   
 }
 //------------------------------------------------
+
+//fix hp<0
