12cbd71f862c9caf9069973ad3fa3da2e569ec44
Egor.yakovlev <Egor.yackovleff@gmail.com>
2018-05-16 00:21:51 +0300

    add labs 0, 2, 3 and report for 1 lab

---
+++ Klimenko/lab3/warrior.h
@@ -0,0 +1,19 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+
+#include "Object.h"
+#include <string.h>
+#include <stdio.h>
+
+class Warior :public Object
+{
+	int force;
+public:
+	Warior(ifstream& fin, const shared_ptr<Crown> t);
+	~Warior();
+	Warior* move(char option);
+	int give_force();
+	// thinkaboutittoo friend istream &operator>>(istream &in, Warior &ob);
+	friend ostream &operator<<(ostream &out, Warior &ob);
+};
+#endif // WARRIOR_H
